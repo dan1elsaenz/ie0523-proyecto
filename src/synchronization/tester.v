632438661d00c69cc45cde8beac66cc12348e10f
Rodrigo Sanchez A <rodrigo.e.sanchez.araya@gmail.com>
2025-12-04 13:19:01 -0600

    Ajuste codigo PUDI

--- src/synchronization/tester.v
+++ src/synchronization/tester.v
@@ -17,7 +17,7 @@ module probador(
 
 initial begin 
     // inicializaciones
-    PUDI = 10'b0100010111;
+    PUDI = 10'b110000_0101;
     clk = 0;
     VALID_PUDI = 0;
     mr_main_reset = 1; 
@@ -31,7 +31,7 @@ initial begin
     PUDI  = 10'b1010010110;// D
     #10
     // comma_cont = 1
-    PUDI = 10'b1010010110;
+    PUDI = 10'b110000_0101;
     //next_state =  COMMA_DETECT
     #10
     PUDI  = 10'b0101101001; // D
