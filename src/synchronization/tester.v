b1200483a819cca04a7b866c9c7d1b961688a44c
Rodrigo Sanchez A <rodrigo.e.sanchez.araya@gmail.com>
2025-12-04 12:52:41 -0600

    Correccion en PUDI
    Signed-off-by: Rodrigo Sanchez A <rodrigo.e.sanchez.araya@gmail.com>

--- src/synchronization/tester.v
+++ src/synchronization/tester.v
@@ -41,7 +41,7 @@ initial begin
     PUDI = 10'b1100000101;
     //next_state =  COMMA_DETECT
     #10
-    PUDI  = 10'b1010010110: // D
+    PUDI  = 10'b1010010110; // D
     #10
     // comma_cont = 3
     PUDI = 10'b1100000101;
