ae875b08af7958aeb0c003aecef99ca8d4dd3e7e
Brandon <brandon.jimenezcampos@ucr.ac.cr>
2025-12-05 19:29:50 -0600

    Cambio de formato del tester del receive

--- src/receive/tester.v
+++ src/receive/tester.v
@@ -34,59 +34,131 @@ module tester (
   // Inicio de pruebas
   initial begin
     rx_clk = 0;
-    mr_main_reset = 0;
+    mr_main_reset = 1;
     sudi = 11'b0;
     sync_status = 0;
 
-    #5 mr_main_reset = 1;
-    #10 mr_main_reset = 0;
-    #10 sync_status = 1;
+    @(posedge rx_clk);
+    mr_main_reset = 0;
 
-    //Prueba en donde envía datos
+    @(posedge rx_clk);
+    sync_status = 1;
 
-    #10 sudi = {`K28_5_10B_RD_N, 1'b1};  // K28.5 RD- (Idle)
-    #10 sudi = {`K28_5_10B_RD_P, 1'b1};  // K28.5 RD+ (Idle)
-    #10 sudi = {`K27_7_10B_RD_N, 1'b1};  // K27.7 RD+ START
+    /*
+    * PRUEBA 1
+    * Está sincronizado, por lo que se envían datos por rxd
+    */
 
-    #20 sudi = {`D11_3_10B_RD_N, 1'b0};  // D11.3 RD- Data válido
-    #10 sudi = {`D23_1_10B_RD_N, 1'b0};  // D23.1 RD+ Data válido
-    #10 sudi = {`D7_4_10B_RD_P, 1'b0};  // D7.4 RD- Data válido
-    #10 sudi = {`D8_6_10B_RD_N, 1'b0};  // D12.5 RD+  Data válido
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_N, 1'b1};  // K28.5 RD- (Idle)
 
-    #10 sudi = {`K29_7_10B_RD_P, 1'b0};  // T
-    #10 sudi = {`K23_7_10B_RD_P, 1'b1};  // R
-    #10 sudi = {`K28_5_10B_RD_N, 1'b0};  // I
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_P, 1'b1};  // K28.5 RD+ (Idle)
 
-    // No está sincronizado, entonces no debe enviar datos por rxd
+    @(posedge rx_clk);
+    sudi = {`K27_7_10B_RD_N, 1'b1};  // K27.7 RD+ START
 
-    #10 sync_status = 0;
+    @(posedge rx_clk);
 
-    #10 sudi = {`K28_5_10B_RD_N, 1'b1};  // K28.5 RD-
-    #10 sudi = {`K28_5_10B_RD_P, 1'b1};  // K28.5 RD+
-    #10 sudi = {`K27_7_10B_RD_N, 1'b1};
+    @(posedge rx_clk);
+    sudi = {`D11_3_10B_RD_N, 1'b0};  // D11.3 RD- Data válido
 
-    #20 sudi = {`D11_3_10B_RD_N, 1'b0};  // D11.3 RD-
-    #10 sudi = {`D23_1_10B_RD_N, 1'b0};  // D23.1 RD+
-    #10 sudi = {`D7_4_10B_RD_P, 1'b0};  // D7.4 RD-
-    #10 sudi = {`D8_6_10B_RD_N, 1'b0};  // D12.5 RD+
+    @(posedge rx_clk);
+    sudi = {`D23_1_10B_RD_N, 1'b0};  // D23.1 RD+ Data válido
 
-    #10 sudi = {`K29_7_10B_RD_P, 1'b0};  // T
-    #10 sudi = {`K23_7_10B_RD_P, 1'b1};  // R
-    #10 sudi = {`K28_5_10B_RD_P, 1'b0};  // I
+    @(posedge rx_clk);
+    sudi = {`D7_4_10B_RD_P, 1'b0};  // D7.4 RD- Data válido
 
-    #10 sync_status = 1;
-    #10 sudi = {`K28_5_10B_RD_N, 1'b1};  // K28.5 RD- (Idle)
-    #10 sudi = {`K28_5_10B_RD_P, 1'b1};  // K28.5 RD+ (Idle)
-    #10 sudi = {`K27_7_10B_RD_N, 1'b1};  // K27.7 RD+ START
+    @(posedge rx_clk);
+    sudi = {`D8_6_10B_RD_N, 1'b0};  // D12.5 RD+  Data válido
 
-    #20 sudi = {`D11_3_10B_RD_N, 1'b0};  // D11.3 RD- Data válido
-    #10 sudi = {`D23_1_10B_RD_N, 1'b0};  // D23.1 RD+ Data válido
-    #10 sudi = {`D7_4_10B_RD_P, 1'b0};  // D7.4 RD- Data válido
-    #10 sudi = {`D8_6_10B_RD_N, 1'b0};  // D12.5 RD+  Data válido
+    @(posedge rx_clk);
+    sudi = {`K29_7_10B_RD_P, 1'b0};  // T
 
-    #10 sudi = {`K29_7_10B_RD_P, 1'b0};  // T
-    #10 sudi = {`K23_7_10B_RD_P, 1'b1};  // R
-    #10 sudi = {`K23_7_10B_RD_P, 1'b0};  // R
+    @(posedge rx_clk);
+    sudi = {`K23_7_10B_RD_P, 1'b1};  // R
+
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_N, 1'b0};  // I
+
+    /*
+    * PRUEBA 2
+    * No está sincronizado, entonces no debe enviar datos por rxd
+    */
+
+    @(posedge rx_clk);
+    sync_status = 0;
+
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_N, 1'b1};  // K28.5 RD-
+
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_P, 1'b1};  // K28.5 RD+
+
+    @(posedge rx_clk);
+    sudi = {`K27_7_10B_RD_N, 1'b1};
+    @(posedge rx_clk);
+
+    @(posedge rx_clk);
+    sudi = {`D11_3_10B_RD_N, 1'b0};  // D11.3 RD-
+
+    @(posedge rx_clk);
+    sudi = {`D23_1_10B_RD_N, 1'b0};  // D23.1 RD+
+
+    @(posedge rx_clk);
+    sudi = {`D7_4_10B_RD_P, 1'b0};  // D7.4 RD-
+
+    @(posedge rx_clk);
+    sudi = {`D8_6_10B_RD_N, 1'b0};  // D12.5 RD+
+
+    @(posedge rx_clk);
+    sudi = {`K29_7_10B_RD_P, 1'b0};  // T
+
+    @(posedge rx_clk);
+    sudi = {`K23_7_10B_RD_P, 1'b1};  // R
+
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_P, 1'b0};  // I
+
+    /*
+    * PRUEBA 3
+    * Se realiza con una terminación /T/R/R
+    */
+
+    @(posedge rx_clk);
+    sync_status = 1;
+
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_N, 1'b1};  // K28.5 RD- (Idle)
+
+    @(posedge rx_clk);
+    sudi = {`K28_5_10B_RD_P, 1'b1};  // K28.5 RD+ (Idle)
+
+    @(posedge rx_clk);
+    sudi = {`K27_7_10B_RD_N, 1'b1};  // K27.7 RD+ START
+
+    @(posedge rx_clk);
+
+    @(posedge rx_clk);
+    sudi = {`D11_3_10B_RD_N, 1'b0};  // D11.3 RD- Data válido
+
+    @(posedge rx_clk);
+    sudi = {`D23_1_10B_RD_N, 1'b0};  // D23.1 RD+ Data válido
+
+    @(posedge rx_clk);
+    sudi = {`D7_4_10B_RD_P, 1'b0};  // D7.4 RD- Data válido
+
+    @(posedge rx_clk);
+    sudi = {`D8_6_10B_RD_N, 1'b0};  // D12.5 RD+  Data válido
+
+    @(posedge rx_clk);
+    sudi = {`K29_7_10B_RD_P, 1'b0};  // T
+
+    @(posedge rx_clk);
+    sudi = {`K23_7_10B_RD_P, 1'b1};  // R
+
+    @(posedge rx_clk);
+    sudi = {`K23_7_10B_RD_P, 1'b0};  // R
 
     #40 $finish;
   end
